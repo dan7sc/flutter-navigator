a41bc92717821c66f38f7313ac80becaec9a702d
dan7sc <su7cheng@gmail.com>
2021-07-07 12:44:52 -0300

    Add logic to nextPage

--- lib/navigator.dart
+++ lib/navigator.dart
@@ -8,29 +8,43 @@ class NavigatorRaroAcademy extends StatefulWidget {
 }
 
 class _NavigatorRaroAcademyState extends State<NavigatorRaroAcademy> {
+  var _count = 0;
+
+  void nextPage() {
+    if (_count == 2) {
+      _count = 0;
+    } else {
+      _count++;
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: nextPage,
       ),
       body: Navigator(
         pages: [
-          MaterialPage(
-            child: Container(
-              color: Colors.red,
+          if (_count == 0)
+            MaterialPage(
+              child: Container(
+                color: Colors.red,
+              ),
             ),
-          ),
-          MaterialPage(
-            child: Container(
-              color: Colors.blue,
+          if (_count == 1)
+            MaterialPage(
+              child: Container(
+                color: Colors.blue,
+              ),
             ),
-          ),
-          MaterialPage(
-            child: Container(
-              color: Colors.green,
+          if (_count == 2)
+            MaterialPage(
+              child: Container(
+                color: Colors.green,
+              ),
             ),
-          ),
         ],
       ),
     );
