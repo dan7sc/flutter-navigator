311dc95147960423ca55fd9d2d153b72b9845b7e
dan7sc <su7cheng@gmail.com>
2021-07-07 12:33:10 -0300

    Add Navigator

---
+++ lib/navigator.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NavigatorRaroAcademy extends StatefulWidget {
+  NavigatorRaroAcademy({Key? key}) : super(key: key);
+
+  @override
+  _NavigatorRaroAcademyState createState() => _NavigatorRaroAcademyState();
+}
+
+class _NavigatorRaroAcademyState extends State<NavigatorRaroAcademy> {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      pages: [
+        MaterialPage(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+        MaterialPage(
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+        MaterialPage(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+      ],
+    );
+  }
+}
